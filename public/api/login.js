67194bf5e19a85d199a48394b837c0472b993442
SScodesKrinjal <krinjalbasnet18@gmail.com>
2025-04-30 18:45:52 -0700

    Create login.js

---
+++ public/api/login.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const router = express.Router();
+
+// Route to render Login page
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+module.exports = router;
